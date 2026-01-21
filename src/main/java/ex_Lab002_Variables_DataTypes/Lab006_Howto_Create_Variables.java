d0fb2eacfa76cd8743c181b47be8f0f015e5f532
beri <sreedivya@gmail.com>
2026-01-21 18:25:05 +0530

    create variables with declaration syntax

---
+++ src/main/java/ex_Lab002_Variables_DataTypes/Lab006_Howto_Create_Variables.java
@@ -0,0 +1,25 @@
+package ex_Lab002_Variables_DataTypes;
+/*
+What is Variable?
+- Store the value (which can be changed in the program).
+- Each variable should be given a** unique name (**identifier).
+- Memory will be allocated for the variable while executing the program.
+- **Value of the variable** can be changed any number of times during the program execution.
+
+*/
+public class Lab006_Howto_Create_Variables {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is variable");
+
+        //how to create variable? (declaration syntax)
+
+        //Data_type variable_name = variable_value;
+
+       int age = 30;
+
+        System.out.println("my age is :" +age);
+
+    }
+}
