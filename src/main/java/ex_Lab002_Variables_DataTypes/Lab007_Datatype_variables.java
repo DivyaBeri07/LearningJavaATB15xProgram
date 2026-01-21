bfa8e802770e5aee5c4929037889dade2ab97bf2
beri <sreedivya@gmail.com>
2026-01-21 18:26:18 +0530

    Adding data type variables

---
+++ src/main/java/ex_Lab002_Variables_DataTypes/Lab007_Datatype_variables.java
@@ -0,0 +1,24 @@
+package ex_Lab002_Variables_DataTypes;
+
+/*       primitive Data types
+byte a = 10;
+short b = 10;
+int c = 10;
+long l = 98745625;
+boolean age = true;
+double sal = 50000;
+float dec = 123;
+char letter = 'A';*/
+
+public class Lab007_Datatype_variables {
+
+    public static void main(String[] args) {
+
+        short b = 10;
+        int c = 10;
+
+        System.out.println(b);
+    }
+
+
+}
