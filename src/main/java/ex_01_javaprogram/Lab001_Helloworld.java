923499186a4773b359260d2bc891a4085303cf52
beri <sreedivya@gmail.com>
2026-01-21 15:33:29 +0530

    adding Helloworld file

--- src/main/java/ex_01_javaprogram/Lab001_Helloworld.java
+++
@@ -1,4 +0,0 @@
-package ex_01_javaprogram;
-
-public class Lab001_Helloworld {
-}
---
+++ src/main/java/ex_Lab001_javaprogram/Lab001_Helloworld.java
@@ -0,0 +1,8 @@
+package ex_Lab001_javaprogram;
+
+public class Lab001_Helloworld {
+     public static void main(String[] args) {
+         System.out.println("Hello World");
+     }
+
+}
