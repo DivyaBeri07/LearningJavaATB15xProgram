c0141d59a3b6cad816debf2ecb06401e9cd2c19a
beri <sreedivya@gmail.com>
2026-01-21 18:22:19 +0530

    variables

---
+++ src/main/java/ex_Lab002_Variables_DataTypes/Lab004_LocalVariables.java
@@ -0,0 +1,30 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab004_LocalVariables {
+
+      /*  public static void main(String[] args) {
+            int a = 10;
+            byte b = 10;
+            short s = 10;
+            long l = 9876543210L;
+            float f = 3.14F;
+            double d = 67.8987654567;
+            boolean b1 = true;
+            char c = 'A';
+        } */
+
+    static boolean a;
+
+    public static void main(String[] args) {
+
+        int $ = 10;
+        int a = 10;
+        int b = 20;
+        char k = 10;
+        char c = 'A';
+
+
+        System.out.println(Lab004_LocalVariables.a);
+    }
+
+}
