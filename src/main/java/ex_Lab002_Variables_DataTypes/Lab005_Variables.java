84ec54b08d986cbdc5dbe8b74b43c01aed636c91
beri <sreedivya@gmail.com>
2026-01-21 18:30:06 +0530

    variable stypes

---
+++ src/main/java/ex_Lab002_Variables_DataTypes/Lab005_Variables.java
@@ -0,0 +1,16 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab005_Variables {
+    int a = 10; // Instance Variable-Inside the class but outside the method
+    static int b = 62; // Static Variable-Variable are declared has Static Variable and it cannot be local variable
+
+    public void test_local() {
+
+        int c = 200; // Local variable-inside the method
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println('c');
+    }
+}
