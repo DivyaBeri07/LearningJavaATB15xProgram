da54f581495f969812022b9f13d9b61965072b01
beri <sreedivya@gmail.com>
2026-01-23 16:47:25 +0530

    float literal

---
+++ src/main/java/ex_Lab004_Literals/Lab014_Literals_float.java
@@ -0,0 +1,14 @@
+package ex_Lab004_Literals;
+
+public class Lab014_Literals_float {
+    // float pi = 3.14f;
+    //float pi4 = 3.14F;
+    //float div = 10.00f;
+    public static void main(String[] args) {
+
+        int Enum = 18;
+//        int enum = 20;
+
+        System.out.println(Enum);
+    }
+}
