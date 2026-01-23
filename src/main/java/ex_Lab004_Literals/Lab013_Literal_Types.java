4b0f7d066b86c0a0e19872bc567ba960a3f4f168
beri <sreedivya@gmail.com>
2026-01-23 16:40:32 +0530

    literals types

---
+++ src/main/java/ex_Lab004_Literals/Lab013_Literal_Types.java
@@ -0,0 +1,16 @@
+package ex_Lab004_Literals;
+
+public class Lab013_Literal_Types {
+    public static void main(String[] args) {
+
+        // Literals types list
+        int age = 29;
+        float deci_num = 116.5f;
+        double sal = 1_00_000;
+        char c = 'A';
+        boolean is_married = true;
+        String name = null;
+
+        System.out.println(c);
+    }
+}
