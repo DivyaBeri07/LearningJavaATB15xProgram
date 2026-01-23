24d9bc13bf15bff33a90fa1082e93aaaba1b77f3
beri <sreedivya@gmail.com>
2026-01-23 16:36:40 +0530

    second commit: added literals

--- src/main/java/ex_Lab004_Literals/Lab012_Literals.java
+++ src/main/java/ex_Lab004_Literals/Lab012_Literals.java
@@ -4,7 +4,7 @@ public class Lab012_Literals {
     public static void main(String[] args) {
 
         int age = 29;
-// Datatype-int, age-variable_name =operator(assigment),18 is identifier(Literal)
+// Datatype-int, age-variable_name =operator(assignment),18 is identifier(Literal)
         System.out.println("This is Literal in the java");
     }
 }
