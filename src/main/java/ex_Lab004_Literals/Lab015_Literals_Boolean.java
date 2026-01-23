b29875348bf4f47fbd2b4b2b7e7313da11012b46
beri <sreedivya@gmail.com>
2026-01-23 16:51:12 +0530

    BooleanLiteral

---
+++ src/main/java/ex_Lab004_Literals/Lab015_Literals_Boolean.java
@@ -0,0 +1,12 @@
+package ex_Lab004_Literals;
+
+public class Lab015_Literals_Boolean {
+    public static void main(String[] args) {
+
+        boolean is_married = false;
+        boolean is_married_DivyaBeri = true;
+
+
+        System.out.println(is_married_DivyaBeri);
+    }
+}
